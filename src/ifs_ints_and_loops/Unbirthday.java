5debe7b9487d75dda7203e50ff9354840d8416f0
League Student <league@iMac-8.attlocal.net>
2019-01-13 11:28:36 -0800

    module 1 1/2

---
+++ src/ifs_ints_and_loops/Unbirthday.java
@@ -0,0 +1,10 @@
+package ifs_ints_and_loops;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+public static void main(String[] args) {
+	JOptionPane.showInputDialog("When were you born?");
+	JOptionPane.showMessageDialog(null,"Happy late birthday");
+}
+}
